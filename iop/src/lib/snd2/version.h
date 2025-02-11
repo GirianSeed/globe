7a5e28688e9dfde445c739f411b53f27cff68f84
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2025-02-11 18:56:15 -0500

    add snd2/version.h

---
+++ iop/src/lib/snd2/version.h
@@ -0,0 +1,10 @@
+#ifndef _VERSION_H_
+#define _VERSION_H_
+
+static unsigned char __ps2_libinfo__[] = {
+	'P','s','I','I',
+	'l','i','b','s','n','d','2',' ',
+	'1','6','0','0'
+};
+
+#endif /* _VERSION_H_ */

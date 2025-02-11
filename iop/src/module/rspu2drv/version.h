9f21751547b28ef179c1e5effd2287646bcb2d87
J.Ingram <26800139+GirianSeed@users.noreply.github.com>
2025-02-11 18:57:10 -0500

    add rspu2drv/version.h

---
+++ iop/src/module/rspu2drv/version.h
@@ -0,0 +1,10 @@
+#ifndef _VERSION_H_
+#define _VERSION_H_
+
+static unsigned char __ps2_libinfo__[] = {
+	'P','s','I','I',
+	'r','s','p','u','2','d','r','v',
+	'1','6','0','0'
+};
+
+#endif /* _VERSION_H_ */
